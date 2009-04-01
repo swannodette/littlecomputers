754c53bb1208f342bdae881fbaf21a87f0d38770
David Nolen <david.nolen@gmail.com>
2009-04-01 17:51:01 -0400

    adding Martin Ceperly's gzip code.

--- Classes/LCFileHelpers.h
+++ Classes/LCFileHelpers.h
@@ -1,3 +1,14 @@
+// if you want zip suport you need to uncomment the ZLIB_SUPPORT define
+// and make sure to add -lz to the Other Linker Flags in the build tab
+// your project target (get info on your project target to find it)
+// Thanks to Martin Ceperly.
+
+#define ZLIB_SUPPORT
+
+#ifdef ZLIB_SUPPORT
+#import "zlib.h"
+#endif
+
 #import <Foundation/Foundation.h>
 
 @interface LCFileHelpers : NSObject
@@ -6,5 +17,6 @@
 
 + (NSArray*) allDocuments;
 + (NSString*) pathToFileInDocuments:(NSString*)fileName;
++ (BOOL) ungzipFile: (NSString *)sourceFilenameString toDestination:(NSString *)destFilenameString;
 
 @end
--- Classes/LCFileHelpers.m
+++ Classes/LCFileHelpers.m
@@ -1,5 +1,7 @@
 #import "LCFileHelpers.h"
 
+#define CHUNK_SIZE 256000 // 256K buffer in memory
+
 @implementation LCFileHelpers
 
 + (NSArray*) allDocuments
@@ -16,5 +18,56 @@
   NSString *documentsDirectory = [paths objectAtIndex:0];
   return [documentsDirectory stringByAppendingPathComponent:fileName];
 }
+
+#ifdef ZLIB_SUPPORT
++ (BOOL) ungzipFile: (NSString *)sourceFilenameString toDestination:(NSString *)destFilenameString
+{
+  const char *sourceFilename = [sourceFilenameString cStringUsingEncoding:NSUTF8StringEncoding];
+  const char *destFilename = [destFilenameString cStringUsingEncoding:NSUTF8StringEncoding];
+  
+  unsigned char out[CHUNK_SIZE];
+  
+  gzFile theFile = gzopen(sourceFilename, "rb");
+  if(theFile == NULL)
+  {
+    NSLog(@"error opening gz file");
+    return NO;
+  }
+  
+  FILE *outFile = fopen(destFilename, "wb");
+  if(outFile == NULL)
+  {
+    NSLog(@"error opening destination file");
+    return NO;
+  }
+  
+  long totalBytes = 0;
+  int readResult = 0;
+  
+  do 
+  {
+    readResult = gzread(theFile, out, CHUNK_SIZE);
+    if(readResult > 0)
+    {
+      fwrite(out, 1, readResult, outFile);
+      totalBytes += readResult;
+    }
+  } while (readResult > 0);
+  
+  gzclose(theFile);
+  fclose(outFile);
+  
+  if(readResult == 0)
+  {
+    NSLog(@"successfully uncompressed gz file, resulting file is %d bytes", totalBytes);
+    return YES;
+  }
+  else
+  {
+    NSLog(@"error reading data from gz file");
+    return NO;
+  }
+}
+#endif
 
 @end
